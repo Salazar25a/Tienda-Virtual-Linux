04a118f142763701b415779578aad80bc3dd38ae
salazar25a <g1ul1anosp2503@gmail.com>
2021-08-11 22:12:46 -0500

    quinta

--- Js/carrusel.js
+++ Js/carrusel.js
@@ -20,16 +20,16 @@ function setImage() {
     var carrusel = document.getElementById("carrusel");
     switch (idxImagen) {
         case 0:
-            carrusel.innerHTML = "<img class='fade-in' src='carrusel/im2.jpg' width=100% height=300/>";
+            carrusel.innerHTML = "<img class='fade-in' src='/carrusel/im2.jpg' width=100% height=300/>";
             break;
         case 1:
-            carrusel.innerHTML = "<img class='fade-in' src='carrusel/im3.jpeg' width=100% height=300/>";
+            carrusel.innerHTML = "<img class='fade-in' src='/carrusel/im3.jpeg' width=100% height=300/>";
             break;
         case 2:
-            carrusel.innerHTML = "<img class='fade-in' src='carrusel/im4.jpg' width=100% height=300/>";
+            carrusel.innerHTML = "<img class='fade-in' src='/carrusel/im4.jpg' width=100% height=300/>";
             break;
         case 3:
-            carrusel.innerHTML = "<img class='fade-in' src='carrusel/im5.jpg' width=100% height=300/>";
+            carrusel.innerHTML = "<img class='fade-in' src='/carrusel/im5.jpg' width=100% height=300/>";
             
             break;
     }
